542e6a814d6690ad20a6ad367930fd580815a6ed
MedBenomar <mohammed.benomar@pxpstudios.com>
2026-04-02 19:16:27 +0100

    AutoGitPullOriginMain

--- hooks/deploy.php
+++ hooks/deploy.php
@@ -1,12 +1,4 @@
 <?php
-
-$output = [];
-$returnCode = 0;
-
-exec('/home/beme2883/dev/bash/sa_deploy.sh 2>&1', $output, $returnCode);
-
 echo "<pre>";
-echo implode("\n", $output);
-echo "\n\nExit Code: " . $returnCode;
+system('/home/beme2883/dev/bash/sa_deploy.sh 2>&1');
 echo "</pre>";
-
