9e6b9dbef212e969887c496922161799df4428b3
MedBenomar <mohammed.benomar@pxpstudios.com>
2026-06-01 00:42:14 +0100

    AutoGitPullOriginMain

--- hooks/deploy.php
+++ hooks/deploy.php
@@ -1,4 +1,6 @@
 <?php
 echo "<pre>";
+// Execute the deploy script and capture its output
+echo "Deploying latest changes... <br>";
 system('/home/huce0783/smartagents.ma/hooks/deploy.sh 2>&1');
 echo "</pre>";
--- hooks/pull.php
+++ hooks/pull.php
@@ -1,3 +1,4 @@
 <?php
-exec('/hooks/deploy.sh');
+echo "pulling latest changes... <br>";
+exec('/home/huce0783/smartagents.ma/hooks/deploy.sh');
 
