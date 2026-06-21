5d73777115f501a2d29a9ca612b17dff23baab93
MedBenomar <mohammed.benomar@pxpstudios.com>
2026-06-21 20:50:06 +0100

    deply Desc

--- hooks/deploy.php
+++ hooks/deploy.php
@@ -4,11 +4,11 @@
         echo "Deploying latest changes... <br>";
 
         
-        system('/home/huce0783/smartagents.ma/hooks/deploy.sh 2>&1'); 
+        system('/home/huce0783/crm.smartagents.ma/hooks/deploy.sh 2>&1'); 
         //2>&1 captures both stdout and stderr and displays it in the browser
         //- The '2' represents the standard error (stderr) stream.
         //- The '>' operator redirects the output of the command.
         //- The '1' represents the standard output (stdout) stream.
 
-    echo "</pre>";
+    echo "</pre>"; 
 ?>    
