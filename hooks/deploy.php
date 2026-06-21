03836531abe7540816d4ac1536a96149889cee7c
MedBenomar <mohammed.benomar@pxpstudios.com>
2026-06-21 21:23:00 +0100

    Fix deploy

--- hooks/deploy.php
+++ hooks/deploy.php
@@ -3,11 +3,10 @@
         // Execute the deploy script and capture its output
         echo "Deploying latest changes... <br>";
         // we need chmod +x public/deploy.sh
-        exec('chmod +x /home/huce0783/crm.smartagents.ma/public/deploy.sh', $output, $return_var);
-        // or better 
-        exec('chmod +x /home/huce0783/crm.smartagents.ma/hooks/deploy.sh', $output, $return_var);
+        exec('chmod +x /home/huce0783/smartagents.ma/hooks/deploy.sh', $output, $return_var);
+
         // then we can execute the deploy.sh script and capture both stdout and stderr
-        system('/home/huce0783/crm.smartagents.ma/public/deploy.sh 2>&1'); 
+        system('/home/huce0783/smartagents.ma/hooks/deploy.sh 2>&1'); 
         //2>&1 captures both stdout and stderr and displays it in the browser
         //- The '2' represents the standard error (stderr) stream.
         //- The '>' operator redirects the output of the command.
